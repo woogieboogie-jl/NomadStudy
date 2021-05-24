b77bb6b0d43fb5d4c1fbcc70a5344441e27f441c
jae <woogieboogie.jl@gmail.com>
2021-05-24 15:43:44 +0900

    server config-7

--- nomad_django/config/settings.py
+++ nomad_django/config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET')
+SECRET_KEY = os.environ.get('DJANGO_SECRET', )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = bool(os.environ.get('DEBUG'))
 
 if DEBUG :
     DATABASES = {
@@ -49,7 +49,7 @@ else:
 
 ALLOWED_HOSTS = [
     ".elasticbeanstalk.com",
-    "127.0.0.1",
+    "localhost",
     ]
 
 
