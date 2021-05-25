9508a7b7c756ef5bff972e0847b56102b89d0d50
jae <woogieboogie.jl@gmail.com>
2021-05-26 01:57:54 +0900

    server config-4

--- nomad_django/config/settings.py
+++ nomad_django/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^r1*thfcalba!wiasu5+n397$b)u7^m^#g^xb7yr(e5ve)+^(*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG'))
+DEBUG = True
 
 if DEBUG :
     DATABASES = {
