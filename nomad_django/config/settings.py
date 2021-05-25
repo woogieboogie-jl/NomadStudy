922015acab7c04b76c05dd28ebf6cb92ca879ffa
jae <woogieboogie.jl@gmail.com>
2021-05-26 02:03:28 +0900

    server config-5

--- nomad_django/config/settings.py
+++ nomad_django/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^r1*thfcalba!wiasu5+n397$b)u7^m^#g^xb7yr(e5ve)+^(*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
