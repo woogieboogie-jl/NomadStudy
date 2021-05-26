8cc1fbba0c9fb4ea050598967cafc705ed12db1c
jae <woogieboogie.jl@gmail.com>
2021-05-27 02:05:57 +0900

    server config-24

--- nomad_django/config/urls.py
+++ nomad_django/config/urls.py
@@ -18,9 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
 
 urlpatterns = [
     path('', include('core.urls', namespace='core')),
@@ -31,9 +28,8 @@ urlpatterns = [
     path('lists/', include('lists.urls', namespace='lists')),
     path('conversations/', include('conversations.urls', namespace='conversations')),
     path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error),
 ]
 
 
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
