f8a36795eb8c79cb53250ff4bd48d903f5ebf98c
jae <woogieboogie.jl@gmail.com>
2021-05-28 02:36:05 +0900

    server config

--- nomad_django/config/settings.py
+++ nomad_django/config/settings.py
@@ -184,7 +184,7 @@ else:
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_deploy")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
