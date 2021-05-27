c78f4f1f33e7b42a75f3246397a3309805499387
jae <woogieboogie.jl@gmail.com>
2021-05-27 15:24:15 +0900

    server config-32

--- nomad_django/config/settings.py
+++ nomad_django/config/settings.py
@@ -168,6 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 AUTH_USER_MODEL = 'users.User'
 
@@ -212,7 +213,6 @@ if not DEBUG:
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     sentry_sdk.init(
     dsn=os.environ.get("SENTRY_URL"),
