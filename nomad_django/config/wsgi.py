f2ae9bbdd52d567af900aea0f31b19f79ac3915e
jae <woogieboogie.jl@gmail.com>
2021-05-28 12:51:26 +0900

    seperate settings

--- nomad_django/config/wsgi.py
+++ nomad_django/config/wsgi.py
@@ -11,6 +11,5 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f"config.settings.{os.environ.get('ENVIRONMENT')}")
 application = get_wsgi_application()
