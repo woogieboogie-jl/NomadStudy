2a26afd7b4c0e3e3e737dfa6f7b30cc8e80a1fa8
jae <woogieboogie.jl@gmail.com>
2021-05-30 13:46:26 +0900

    server config

--- nomad_django/config/settings/production.py
+++ nomad_django/config/settings/production.py
@@ -38,16 +38,17 @@ ALLOWED_HOSTS = [
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-DEFALUT_FILE_STORAGE = "config.custom_storages.UploadStorage"
-STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "nomad-django-staticbuckets"
 AWS_DEFAULT_ACL = "public-read"
-
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
